7e965739c85259d0f09b08130367840c635e3881
Santanu Mohapatra <99542723+Santanu55555@users.noreply.github.com>
2022-11-09 17:57:54 +0530

    ok

--- task_manager/src/pages/Dashboard.jsx
+++ task_manager/src/pages/Dashboard.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import { Sidebar } from "../components/Sidebar";
 
 export const Dashboard = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <Box>
+      <Sidebar  w="25%"h="100%" position="fixed"/>
+      <Box w="75%">
+        <Navbar/>
+        <Box></Box>
+      </Box>
+    </Box>
+  );
+};
